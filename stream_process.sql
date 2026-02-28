f28fa583c661132ee4823997e0c0982b33e9322c
zablon <zablonoigo51@gmail.com>
2026-02-28 23:24:06 +0300

    test quick query

--- stream_process.sql
+++ stream_process.sql
@@ -10,3 +10,18 @@ CREATE STREAM BIKE_HIRE_SRC (
     KAFKA_TOPIC='bike.hire',
     VALUE_FORMAT='AVRO'
 );
+
+
+-- Quick query 
+SET 'auto.offset.reset' = 'earliest';
+
+SELECT 
+    TIMESTAMPTOSTRING(ROWTIME,'yyyy-MM-dd HH:mm:ss','Europe/London') AS LASTUPDATE,
+    ID,
+    NAME,
+    BIKES,
+    EMPTY_DOCKS,
+    TOTAL_DOCKS
+FROM BIKE_HIRE_SRC
+EMIT CHANGES
+LIMIT 5;
