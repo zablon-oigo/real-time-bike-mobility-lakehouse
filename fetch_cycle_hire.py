bd8415d25f9eba41d65b8d890527f862c62fb4e4
zablon <zablonoigo51@gmail.com>
2026-03-03 17:49:03 +0300

    load avro schema

---
+++ fetch_cycle_hire.py
@@ -0,0 +1,19 @@
+from confluent_kafka import SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import StringSerializer
+import xml.etree.ElementTree as ET
+import httpx
+import logging
+
+
+SCHEMA_REG_URL = "http://localhost:8081"
+TOPIC = "bike.hire"
+BASE_URL = "https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml"
+
+# Load Avro schema
+with open("schema.avro", "r") as f:
+    schema_str = f.read()
+
+
+
